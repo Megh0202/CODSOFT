387d2ec45c8e361096054889c41f17ce98448470
Megh0202 <142076371+Megh0202@users.noreply.github.com>
2025-03-05 21:36:34 +0530

    Password.py

--- password.py
+++ password.py
@@ -1,16 +1,21 @@
-import random
-import string
+import random  # Import the random module for generating random choices
+import string # Import the string module to access letters, digits, and special char
 
 def generate_password(length):
+    # Define the character set
     characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for _ in range(length))
-    return password
-try:
-    length = int(input("Enter the desired length of the password: "))
-    if length <= 0:
-        print("Please enter a positive number.")
-    else:
-        password = generate_password(length)
-        print("Generated Password:", password)
-except ValueError:
-    print("Invalid input.. Please enter a valid number.")
+     # Use random.choices() to generate a password of the given length
+    return ''.join(random.choices(characters, k=length))
+
+def main():
+    try:
+        length = int(input("Enter the desired length of the password: "))
+        if length <= 0:
+            print("Please enter a positive number.")
+        else:
+            print("Generated Password:", generate_password(length))
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+# Run the main function
+if __name__ == "__main__":
+    main()
